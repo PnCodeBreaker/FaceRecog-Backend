21f2b498f6341d2d04a9a3ad9c713f07049639c6
PnCodeBreaker <prithwishnp.n@gmail.com>
2020-07-15 10:49:32 +0530

    changing db connection

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ const knex = require('knex');
 const db = knex({
         client: 'pg',
         connection: {
-        host : '127.0.0.1',
-        user : 'postgres',
-        password : 'pndbbreaker',
-        database : 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl:true
     }
   });
 
